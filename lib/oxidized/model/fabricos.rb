edfe0029dd496772b7209b9738c6a8e29cb8b158
Jarkko Oranen <oranenj@iki.fi>
2020-04-23 10:44:19 +0300

    fabricos: Add secret filtering support and allow dashes in hostnames

--- lib/oxidized/model/fabricos.rb
+++ lib/oxidized/model/fabricos.rb
@@ -1,12 +1,25 @@
 class FabricOS < Oxidized::Model
   # Brocade Fabric OS model #
-  ## FIXME: Only ssh exec mode support, no telnet, no ssh screenscraping
 
-  prompt /^([\w]+:+[\w]+[>]\s)$/
+  prompt /^([\w-]+:+[\w-]+[>]\s)$/
   comment '# '
+
+  cmd :all do |cfg|
+    cfg = cfg.cut_both
+    cfg = cfg + "\n"
+  end
+
+  cmd :secret do |cfg|
+    cfg.gsub!(/([s]Secret:)\S+/, '\1 SECRET REMOVED')
+    cfg.gsub!(/(http.sl.user:)\S+/, '\1 SECRET REMOVED')
+  end
 
   cmd 'chassisShow' do |cfg|
     comment cfg.each_line.reject { |line| line.match(/Time Awake:/) || line.match(/Power Usage \(Watts\):/) || line.match(/Time Alive:/) || line.match(/Update:/) }.join
+  end
+
+  cmd 'version' do |cfg|
+    comment cfg
   end
 
   cmd 'configShow -all' do |cfg|
@@ -15,6 +28,7 @@ class FabricOS < Oxidized::Model
   end
 
   cfg :ssh do
-    exec true # don't run shell, run each command in exec channel
+    pre_logout 'exit'
   end
 end
+
